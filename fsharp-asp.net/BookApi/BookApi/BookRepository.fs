e136046b0bc330c4f3077f02bb0a70396fbe5e03
Dumitru-Daniel Mititelu <mttl_daniel@yahoo.com>
2021-09-17 15:29:09 +0300

    Add f# project

---
+++ fsharp-asp.net/BookApi/BookApi/BookRepository.fs
@@ -0,0 +1,43 @@
+namespace BookApi
+
+open System
+
+type Book =
+    { Id: Guid
+      Title: string
+      Author: string
+      Pages: int}
+
+type BookRepository() =
+    static let mutable books: Map<Guid, Book> = Map.empty
+
+    member this.Get() =
+        books |> Map.toSeq |> Seq.map snd |> Seq.toList
+
+    member this.GetById(id: Guid) = 
+        let result =  books.TryFind(id)
+        result
+
+    member this.Add(book: Book) =
+        let id = Guid.NewGuid()
+        let book = {
+             Id = id;
+             Title = book.Title;
+             Author = book.Author; 
+             Pages = book.Pages
+        }
+        books <- books.Add(id, book)
+        book
+
+    member this.Delete(id: Guid) =
+        books <- books.Remove(id)
+
+    member this.Update(id: Guid, book: Book) = 
+        books <- books.Remove(id)
+        let bookToAdd = {
+              Id = id;
+              Title = book.Title;
+              Author = book.Author; 
+              Pages = book.Pages
+         }
+        books <- books.Add(id, bookToAdd)
---
+++ fsharp-asp.net/BookApi/BookApi/Controllers/BookController.fs
@@ -0,0 +1,36 @@
+﻿namespace BookApi.Controllers
+
+open Microsoft.AspNetCore.Mvc
+open BookApi
+open System
+
+[<ApiController>]
+[<Route("api/[controller]")>]
+type BookController (bookRepository : BookRepository) =
+    inherit ControllerBase()
+
+    [<HttpGet>]
+    member _.Get() =
+        bookRepository.Get()
+
+    [<HttpGet("{id}")>]
+    member _.GetById(id: Guid) : IActionResult = 
+        match bookRepository.GetById(id) with
+        | Some book -> new ObjectResult(book) :> IActionResult 
+        | None -> NotFoundResult() :> IActionResult 
+
+
+    [<HttpPost>]
+    member _.Post(book: Book) : IActionResult =
+        let addedBook = bookRepository.Add(book)
+        CreatedAtRouteResult( "/" + addedBook.Id.ToString(), addedBook) :> IActionResult 
+
+    [<HttpDelete("{id}")>]
+    member _.Delete(id: Guid) : IActionResult = 
+        bookRepository.Delete(id)
+        NoContentResult() :> IActionResult
+
+    [<HttpPut("{id}")>]
+    member _.Update(id: Guid, book: Book) : IActionResult  =
+        bookRepository.Update(id, book)
+        NoContentResult() :> IActionResult
